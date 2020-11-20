26c5958cc934f340d8b3e4a01b77dbd8fe8be052
Olaf Otterbach <olaf.otterbach@gmx.de>
2020-11-20 19:23:50 +0100

    Adding overlapping tests. +semver:minor

--- IGraphics.Tests/Mathmatics/IntersectionMathTests.cs
+++ IGraphics.Tests/Mathmatics/IntersectionMathTests.cs
@@ -7,9 +7,39 @@ namespace IGraphics.Tests
     public class IntersectionMathTests
     {
         [Fact]
-        public void AreLinesBoundedBoxesOverlappedTest()
+        public void AreLinesBoundedBoxesOverlappedTest1()
         {
-            Assert.True(IntersectionMath.AreLinesBoundedBoxesOverlapped(1,1, 10,10, 1,10, 10,1));
+            Assert.True(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 1, 10, 10, 1));
+        }
+
+        [Fact]
+        public void AreLinesBoundedBoxesOverlappedTest2()
+        {
+            Assert.True(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 5-1, 5+1, 5+1, 5-1));
+        }
+
+        [Fact]
+        public void AreLinesBoundedBoxesOverlappedTest3()
+        {
+            Assert.False(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 20, 10, 30, 1));
+        }
+
+        [Fact]
+        public void AreLinesBoundedBoxesOverlappedTest4()
+        {
+            Assert.False(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 10, 10, 20, 1));
+        }
+
+        [Fact]
+        public void AreLinesBoundedBoxesOverlappedTest5()
+        {
+            Assert.False(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 1, 30, 10, 20));
+        }
+
+        [Fact]
+        public void AreLinesBoundedBoxesOverlappedTest6()
+        {
+            Assert.False(IntersectionMath.AreLinesBoundedBoxesOverlapped(1, 1, 10, 10, 1, 20, 10, 10));
         }
     }
 }
